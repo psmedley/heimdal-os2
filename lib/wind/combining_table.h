73bc6a8b35fafa8ba3f396a0679cffdaab9275dd
Paul Smedley <paul@smedley.id.au>
2024-07-13 16:52:04 +0930

    Import 7.7.0 source
    Signed-off-by: Paul Smedley <paul@smedley.id.au>

---
+++ lib/wind/combining_table.h
@@ -0,0 +1,17 @@
+/* ./combining_table.h */
+/* Automatically generated at 2019-06-07T02:26:41.530328 */
+
+#ifndef COMBINING_TABLE_H
+#define COMBINING_TABLE_H 1
+
+#include <krb5-types.h>
+
+struct translation {
+  uint32_t key;
+  unsigned combining_class;	
+};
+
+extern const struct translation _wind_combining_table[];
+
+extern const size_t _wind_combining_table_size;
+#endif /* COMBINING_TABLE_H */
